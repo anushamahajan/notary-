3fe0940240f76aaccdae387f12ec097cc8e48adc
Chaitanya Anand <chaitanya.anand@gmail.com>
2024-07-16 01:08:06 +0530

    slider imgs

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,50 +1,22 @@
-'use client'
-import React from 'react';
-import styled, { keyframes } from 'styled-components';
-
-const marquee = keyframes`
-  0% {
-    transform: translateX(0%);
-  }
-  100% {
-    transform: translateX(-100%);
-  }
-`;
-
-const MarqueeContainer = styled.div`
-  overflow: hidden;
-  padding : 3rem 0;
-  white-space: nowrap;
-  width: 100%;
-  background: #fff;
-  position: relative;
-`;
-
-const MarqueeContent = styled.div`
-  display: inline-block;
-  animation: ${marquee} 20s linear infinite;
-`;
-
-const Slide = styled.img`
-  display: inline-block;
-  width: 25%;
-  background-color: red;
-  object-fit: contain;
-  margin-right: 32px; /* Adjust spacing between slides */
-`;
+"use client";
+import React from "react";
+import Marquee from "react-fast-marquee";
 
 const Sliderss = () => {
   return (
-    <MarqueeContainer>
-      <MarqueeContent>
-        {Array.from({ length: 10 }).map((_, index) => (
-          <Slide key={index} src={`/slider/${index + 1}.png`} alt={`slide-${index}`} />
-        ))}
-        {Array.from({ length: 10 }).map((_, index) => (
-          <Slide key={index + 10} src={`/slider/${index + 1}.png`} alt={`slide-${index}`} />
-        ))}
-      </MarqueeContent>
-    </MarqueeContainer>
+    <div className="py-10">
+
+    <Marquee speed={100}   gradient gradientColor={"white"} gradientWidth="10%">
+      {Array.from({ length: 21 }).map((_, index) => (
+        <img
+        className="md:h-52 object-contain h-36"
+        key={index}
+        src={`/slider/${index + 1}.png`}
+        alt={`slide-${index}`}
+        />
+      ))}
+    </Marquee>
+      </div>
   );
 };
 
