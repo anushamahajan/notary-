ed704c9a3e4f7fdaeae86320027f460ab5e1e267
Chaitanya Anand <chaitanya.anand@gmail.com>
2024-07-16 20:50:44 +0530

    email

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -1,14 +1,39 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 import { Button } from './ui/button'
 import { Montserrat } from 'next/font/google';
+import emailjs from "@emailjs/browser";
+
 const mont = Montserrat({ subsets: ["latin"] });
 
 const ContactUs = () => {
+  const [name, setName] = React.useState('');
+  const [phone, setPhone] = React.useState('');
+  const [message, setMessage] = React.useState('');
+
+  useEffect(() => emailjs.init("n98FMVTpQ5jgJ6ya6"), []);
+  const sendEmail = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await emailjs
+      .send("service_13xmfel", "template_efxkuwo", {
+        name: name,
+        mobile: phone,
+        message : message,
+      })
+      .then(
+        () => {
+          alert("We have recieved your message. We will get back to you soon!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
   return (
     <div id='contactus' className="flex flex-col md:flex-row items-center justify-center min-h-screen gap-9 my-6 md:my-0">
         <div className="md:w-1/2 w-5/6 max-w-lg bg-white p-8 rounded-lg shadow-lg">
           <h2 className={`text-3xl font-bold mb-6 ${mont.className}`}>Contact Us</h2>
-          <form action="#" method="POST">
+          <form  onSubmit={sendEmail}>
             <div className="mb-4">
               <label className="block text-xl font-medium text-gray-700">
                 Name
@@ -17,6 +42,8 @@ const ContactUs = () => {
                 type="text"
                 id="name"
                 name="name"
+                value={name}
+                onChange={(e)=>setName(e.target.value)}
                 placeholder="Enter your name"
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 
                         rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -27,8 +54,9 @@ const ContactUs = () => {
                 Phone
               </label>
               <input
-                type="email"
                 id="phone"
+                value={phone}
+                onChange={(e)=>setPhone(e.target.value)}
                 name="phone"
                 placeholder="Enter your Mobile Number"
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500
@@ -42,6 +70,8 @@ const ContactUs = () => {
               <textarea
                 id="message"
                 name="message"
+                value={message}
+                onChange={(e)=>setMessage(e.target.value)}
                 rows={4}
                 placeholder="Type your message here..."
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
--- src/components/requestCallDialog.tsx
+++ src/components/requestCallDialog.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
 import {
   Dialog,
   DialogClose,
@@ -10,32 +11,62 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-
+import emailjs from "@emailjs/browser";
 type Props = {
   open: boolean;
   setOpen: (open: boolean) => void;
 };
 
 const RequestDialog = ({ open, setOpen }: Props) => {
+
+  const [name, setName] = React.useState("");
+  const [mobile, setMobile] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  useEffect(() => emailjs.init("n98FMVTpQ5jgJ6ya6"), []);
+  const sendEmail = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await emailjs
+      .send("service_13xmfel", "template_u9s253e", {
+        name: name,
+        mobile: mobile,
+        email: email,
+      })
+      .then(
+        () => {
+          alert("We will get back to you soon!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
     <div>
       {" "}
       <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent onOpenAutoFocus={(e) => e.preventDefault()} className="bg-purple-700 text-white w-5/6 rounded-md">
+        <DialogContent
+          onOpenAutoFocus={(e) => e.preventDefault()}
+          className="bg-purple-700 text-white w-5/6 rounded-md"
+        >
           <DialogHeader>
-            <DialogTitle >Request a callback!</DialogTitle>
+            <DialogTitle>Request a callback!</DialogTitle>
             <DialogDescription className="text-gray-200">
               Need help? Request a callback and we will get back to you as soon
               as possible.
             </DialogDescription>
           </DialogHeader>
-
-          <div className="space-y-4 flex flex-col text-black">
-            <Input placeholder="Name" />
-            <Input placeholder="Phone Number" />
-            <Input placeholder="Email" />
-            <Button className="w-min" variant={'secondary'}>Submit</Button>
-          </div>
+          <form
+            onSubmit={sendEmail}
+            className="space-y-4 flex flex-col text-black"
+          >
+            <Input placeholder="Name" name="from_name" value={name} onChange={(e)=>setName(e.target.value)} />
+            <Input placeholder="Phone Number" name="mobile" value={mobile} onChange={(e)=>setMobile(e.target.value)} />
+            <Input placeholder="Email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+            <Button className="w-min" variant={"secondary"} type="submit">
+              Submit
+            </Button>
+          </form>
         </DialogContent>
       </Dialog>
     </div>
